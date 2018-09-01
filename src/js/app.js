4bfd06ef01d941d952cecaddbb3689207b04f8f3
z3by <zoaby.am@gmail.com>
2018-09-01 07:59:31 +0300

    final sprint

--- src/js/app.js
+++ src/js/app.js
@@ -8,224 +8,159 @@ App = {
 
 
   init: function () {
-
+    App.bindEvents();
     return App.initWeb3();
   },
 
-
-
-  initWeb3: function () {
-    // Is there an injected web3 instance?
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-    } else {
-      // If no injected web3 instance is detected, fall back to Ganache
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-    }
-    web3 = new Web3(App.web3Provider);
-    return App.initContract();
-  },
-
-
-
-
-  initContract: function () {
-    $.getJSON('ERC721Token.json', function (data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var LandArtifact = data;
-      App.contracts.Land = TruffleContract(LandArtifact);
-
-      // Set the provider for our contract
-      App.contracts.Land.setProvider(App.web3Provider);
-
-      App.createContractInstance();
-      return App.bindEvents();
-
-    });
-  },
-
-
-
-  createContractInstance() {
-    App.contracts.Land.deployed()
-      .then(function (instance) {
-        App.contractInstance = instance;
-      });
-
-  },
-
-
-
-
-  bindEvents: function () {
-    $('.approve').click(App.approve);
-    $('.approve-all').click(App.approveAll);
-    $('.is-approved').click(App.isApproved);
-    $('.is-approved-all').click(App.isApprovedForAll);
-    $('.balance-btn').click(App.getBalance);
-    $('.is-exist').click(App.isExist);
-    $('.owner-by-id').click(App.ownerById);
-    $('.transfer-btn').click(App.transfer);
-    $('.safe-transfer-btn').click(App.safeTransfer);
-    $('.total-count').click(App.totalSupply);
-  },
-
-
-
-
-  // approve someone to deal with one of your lands;
-  approve: function () {
-    const key = $('#approve-key').val();
-    const id = $('#approve-id').val();
-
-    App.contractInstance.approve(key, id).then(() => {
-      App.setMessage();
-      App.msg.text('approved');
-    });
-  },
-
-
-
-
-
-
-  // approve someone to deal with all of your lands;
-  approveAll: function () {
-    const key = $('#approve-all-key').val();
-    App.contractInstance.setApprovalForAll(key, true).then(() => {
-      App.setMessage();
-      App.msg.text('approved for all');
-    });
-  },
-
-
-
-  // check who has the permession for one land
-  isApproved: function () {
-    const id = $('#is-approved-id').val();
-
-    App.contractInstance.getApproved(id).then(approved => {
-      console.log(approved);
-    });
-
-  },
-
-
-
-
-  // check if someone is approved for all of your lands
-  isApprovedForAll: function () {
-    const first = $('#is-approved-all-first').val();
-    const second = $('#is-approved-all-second').val();
-
-    App.contractInstance.isApprovedForAll(first, second).then((approved) => {
-      App.setMessage();
-      if (approved) {
-        App.msg.text('this user is approved for all lands');
-      } else {
-        App.msg.text('this user is not approved for all lands');
-
-      }
-    });
-
-  },
-
-
-
-  //check how many lands someone has
-  getBalance: function () {
-    const key = $('#balance-key').val();
-    App.contractInstance.balanceOf(key).then((balance) => {
-      App.setMessage();
-      App.msg.text(balance.toNumber());
-    });
-
-
-  },
-
-
-
-
-  // check if the land is exist
-  isExist: function () {
-    const id = $('#exist-id').val();
-    App.contractInstance.exists(id).then(exists => {
-      App.setMessage();
-      if (exists) {
-        App.msg.text('the records for this land is exists');
-      } else {
-        App.msg.text('the records for this land is not exists');
-
-      }
-    });
-
-  },
-
-
-  setMessage: function () {
+  setMessage: function (text) {
     App.msg.show();
+    App.msg.text(text);
     setTimeout(() => {
       App.msg.hide();
     }, 3000);
   },
 
 
+  bindEvents: function () {
+    $('.approve').click(App.approve);
+    $('.approve-all').click(App.setApprovalForAll);
+    $('.is-approved').click(App.getApproved);
+    $('.is-approved-all').click(App.isApprovedForAll);
+    $('.balance-btn').click(App.balanceOf);
+    $('.is-exist').click(App.exists);
+    $('.owner-by-id').click(App.ownerOf);
+    $('.transfer-btn').click(App.transferFrom);
+    $('.safe-transfer-btn').click(App.safeTransferFrom);
+    $('.total-count').click(App.totalSupply);
+  },
+
+  /**
+   * set the HTTP Provider for the web3 to 
+   * use the same network that you deployed your contract to
+   */
+  initWeb3: function () {
+    // replace me :)
+  },
 
 
-  // enter the land id to get it's owner
-  ownerById: function () {
-    const id = $('#owner-id').val();
 
-    App.contractInstance.exists(id).then((exists) => {
-      if (exists) {
-        App.contractInstance.ownerOf(id).then((owner) => {
-          App.setMessage();
-          App.msg.text('the owner public key is ' + owner);
-        });
-      } else {
-        App.setMessage();
-        App.msg.text('no such land records');
-      }
+  /**
+   * use truffle to instantiate the contract
+   * save the contract in contract object up ^
+   */
+  initContract: function () {
+    $.getJSON('ERC721Token.json', function (data) {
+      // replace me :)
     });
+  },
+
+
+  /**
+   * after instantiating the contract you need to save the deployed instance in contractInstance up^
+   * when you get the deployed instance you can interact with the contract by calling the methods via the contract instance.
+   */
+  createContractInstance() {
+    // replace me :)
   },
 
 
 
 
-  // Transfer Ownership of a land from someone to someone else
-  transfer: function () {
+
+
+
+  // call the method approve that is located in the smart contract and show msg to the final user;
+  approve: function () {
+    const key = $('#approve-key').val();
+    const id = $('#approve-id').val();
+    // replace me :)
+  },
+
+
+
+
+
+
+  // call the setApprovalForAll method that is located in the smart contract and show msg to the final user;
+  setApprovalForAll: function () {
+    const key = $('#approve-all-key').val();
+    // replace me :)
+  },
+
+
+
+  // call the getApproved method that is located in the in smart contract and show msg to the user
+  getApproved: function () {
+    const id = $('#is-approved-id').val();
+    // replace me :)
+  },
+
+
+
+
+  // call isApprovedForAll method and show result msg to the final user
+  isApprovedForAll: function () {
+    const first = $('#is-approved-all-first').val();
+    const second = $('#is-approved-all-second').val();
+    // replace me :)
+  },
+
+
+
+  // call balanceOf method and show the balance as message to the final user
+  balanceOf: function () {
+    const key = $('#balance-key').val();
+    // replace me :)
+  },
+
+
+
+
+  // call exists method that is located in the smart contract and show message to the final user
+  exists: function () {
+    const id = $('#exist-id').val();
+    // replace me :)
+  },
+
+
+
+
+
+
+  // call ownerOf method and show message to the final user
+  ownerOf: function () {
+    const id = $('#owner-id').val();
+    // replace me :)
+  },
+
+
+
+
+  // call transferFrom method that is located in the smart contract and show message to the final user
+  transferFrom: function () {
     const from = $('#transfer-from').val();
     const to = $('#transfer-to').val();
     const id = $('#transfer-id').val();
-    App.contractInstance.transfer(from, to, id);
-
-
+    // replace me :)
   },
 
 
 
-
-  // safe Transfer Ownership of a land from someone to someone else
-  safeTransfer: function () {
+  // call safeTransferFrom method that is located in the smart contract and show message to the final user
+  safeTransferFrom: function () {
     const from = $('#safe-transfer-from').val();
     const to = $('#safe-transfer-to').val();
     const id = $('#safe-transfer-id').val();
-    App.contractInstance.safeTransferFrom(from, to, id).then(done => {
-      console.log(done);
-    });
+    // replace me :)
   },
 
 
 
 
 
-  // safe Transfer Ownership of a land from someone to someone else
-
+  // call totalSupply method in the smart contract instance and show the result as message to the final user
   totalSupply: function () {
-    App.contractInstance.totalSupply().then(data => {
-      App.setMessage();
-      App.msg.text('there is ' + (data.toNumber()) + ' registerd lands');
-    });
+    // replace me :)
   },
 
 };
